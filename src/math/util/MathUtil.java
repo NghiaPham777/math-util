9ec4f1903d22ad4bb147ced0ef7719080fa185cb
NghiaPham777 <nghiapham2704@gmail.com>
2021-03-16 17:31:59 +0700

    1st uploadgit config --global user.name NghiaPham777!

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author Vinacenter
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1st upload form Local to GitHub!!!");
+        System.out.println("Welcome to GitHub");
+    }
+    
+}
